7b81d4c77cf3c483023a424fdddd4e297529912c
Kamran Sayed <Kamran.Sayed@teknopoint.local>
2023-03-21 19:41:43 +0530

    Added logic for calculating emi and rendering the emi on the front on input change

--- js/biz.js
+++
@@ -1,5 +0,0 @@
-var bizObj = {};
-
-(function () {});
-
-Object.freeze(bizObj);
--- js/render.js
+++ js/render.js
@@ -2,14 +2,69 @@ const renderObj = {};
 
 (function () {
   const amountSlider = document.getElementById("loan-amount-range");
-  const durationSlider = document.getElementById("loan-duration-range");
-  const interestSlider = document.getElementById("loan-interest-range");
-  const sliderArray = [amountSlider, durationSlider, interestSlider];
-
   const amountInput = document.getElementById("loan-amount-input");
+  let amount = amountSlider.value;
+
+  const durationSlider = document.getElementById("loan-duration-range");
   const durationInput = document.getElementById("loan-duration-input");
+
+  const interestSlider = document.getElementById("loan-interest-range");
   const interestInput = document.getElementById("loan-interest-input");
-  const inputArray = [amountInput, durationInput, interestInput];
+
+  const emiAmountEl = document.getElementById("monthly-emi-amount");
+
+  let calcData = {
+    amount: amountSlider.value,
+    duration: durationSlider.value,
+    interestRate: interestSlider.value,
+  };
+
+  renderObj.emiData = calculateEMI(
+    parseFloat(calcData.amount),
+    parseFloat(calcData.duration),
+    parseFloat(calcData.interestRate)
+  );
+
+  emiAmountEl.innerText = "₹" + Math.round(renderObj.emiData.emi) + "*";
+
+  const ctx = document.getElementById("myChart");
+  const chartOptions = {
+    type: "pie",
+    data: {
+      datasets: [
+        {
+          data: [
+            renderObj.emiData.totalAmount,
+            renderObj.emiData.totalInterest,
+          ],
+        },
+      ],
+    },
+    options: {},
+  };
+  const pieChart = new Chart(ctx, chartOptions);
+
+  function calculateEMI(principalAmount, numberOfYears, interestPercent) {
+    let durationInMonths = numberOfYears * 12;
+    let interestRatePerMonth = interestPercent / 12 / 100;
+
+    let emiAmount =
+      (principalAmount *
+        interestRatePerMonth *
+        Math.pow(1 + interestRatePerMonth, durationInMonths)) /
+      (Math.pow(1 + interestRatePerMonth, durationInMonths) - 1);
+
+    let totalPayableAmount = emiAmount * durationInMonths;
+    let totalInterestPayable = totalPayableAmount - principalAmount;
+
+    return {
+      emi: emiAmount,
+      totalAmount: totalPayableAmount,
+      totalInterest: totalInterestPayable,
+    };
+  }
+
+  renderObj.calcData = calcData;
 
   function updateInputVal(inputEl, rangeSliderEl) {
     // set range slider's default value to input's default value
@@ -18,6 +73,23 @@ const renderObj = {};
     // update input value on slider change
     rangeSliderEl.addEventListener("input", function (event) {
       inputEl.value = event.target.value;
+
+      if (this.classList.contains("loan-amount-range")) {
+        renderObj.calcData.amount = this.value;
+      } else if (this.classList.contains("loan-duration-range")) {
+        renderObj.calcData.duration = this.value;
+      } else {
+        renderObj.calcData.interestRate = this.value;
+      }
+
+      renderObj.emiData = calculateEMI(
+        parseFloat(calcData.amount),
+        parseFloat(calcData.duration),
+        parseFloat(calcData.interestRate)
+      );
+
+      emiAmountEl.innerText = "₹" + Math.round(renderObj.emiData.emi) + "*";
+      pieChart.update();
     });
 
     inputEl.addEventListener("change", function (event) {
@@ -39,6 +111,7 @@ const renderObj = {};
       }
 
       rangeSliderEl.value = event.target.value;
+      console.log(amount, rangeSliderEl.value);
     });
 
     inputEl.addEventListener("input", function (event) {
@@ -47,17 +120,15 @@ const renderObj = {};
       } else {
         rangeSliderEl.value = rangeSliderEl.getAttribute("min");
       }
-      console.log(event.target.value, rangeSliderEl.value);
     });
   }
 
   renderObj.updateInputVal = updateInputVal;
 
-  if (inputArray.length === sliderArray.length) {
-    for (let i = 0; i < inputArray.length; i++) {
-      renderObj.updateInputVal(inputArray[i], sliderArray[i]);
-    }
-  }
+  renderObj.updateInputVal(amountInput, amountSlider);
+  renderObj.updateInputVal(durationInput, durationSlider);
+  renderObj.updateInputVal(interestInput, interestSlider);
 })();
 
+console.log(renderObj);
 // Object.freeze(renderObj);
