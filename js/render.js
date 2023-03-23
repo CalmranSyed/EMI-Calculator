c99fb24732441d880044ece4e072461ef3b6926e
Kamran Sayed <Kamran.Sayed@teknopoint.local>
2023-03-23 19:30:57 +0530

    Added code for updating EMI on changing values

--- js/render.js
+++ js/render.js
@@ -1,6 +1,7 @@
 const renderObj = {};
 
 (function () {
+  // HTML elements
   const amountSlider = document.getElementById("loan-amount-range");
   const amountInput = document.getElementById("loan-amount-input");
   let amount = amountSlider.value;
@@ -12,37 +13,102 @@ const renderObj = {};
   const interestInput = document.getElementById("loan-interest-input");
 
   const emiAmountEl = document.getElementById("monthly-emi-amount");
+  const totalAmountEl = document.getElementById("total-amount-payable");
+  const totalInterestEl = document.getElementById("total-interest-payable");
 
-  let calcData = {
+  const chartContainerEl = document.getElementById("chart-container");
+
+  // inputed data from the calc
+  renderObj.calcData = {
     amount: amountSlider.value,
     duration: durationSlider.value,
     interestRate: interestSlider.value,
   };
 
+  // calculated EMI data from the initial calc data
   renderObj.emiData = calculateEMI(
-    parseFloat(calcData.amount),
-    parseFloat(calcData.duration),
-    parseFloat(calcData.interestRate)
+    parseFloat(renderObj.calcData.amount),
+    parseFloat(renderObj.calcData.duration),
+    parseFloat(renderObj.calcData.interestRate)
   );
 
-  emiAmountEl.innerText = "₹" + Math.round(renderObj.emiData.emi) + "*";
+  // render EMI data from the inital input data
+  renderEmiData(
+    renderObj.emiData.emi,
+    renderObj.emiData.totalAmount,
+    renderObj.emiData.totalInterest
+  );
 
-  const ctx = document.getElementById("myChart");
-  const chartOptions = {
-    type: "pie",
-    data: {
-      datasets: [
-        {
-          data: [
-            renderObj.emiData.totalAmount,
-            renderObj.emiData.totalInterest,
-          ],
-        },
-      ],
-    },
-    options: {},
+  // colors for pie chart and wherever else needed
+  renderObj.color = {
+    yellow: "#d8e024",
+    blue: "#2e93eb",
   };
-  const pieChart = new Chart(ctx, chartOptions);
+
+  // options for the graph / pie-chart
+  const chartOptions = {
+    chart: {
+      plotBackgroundColor: null,
+      plotBorderWidth: null,
+      plotShadow: false,
+      type: "pie",
+    },
+    credits: { enabled: false },
+
+    title: {
+      text: "",
+      align: "left",
+    },
+    tooltip: {
+      enabled: false,
+    },
+    accessibility: {
+      point: {
+        valueSuffix: "%",
+      },
+    },
+    plotOptions: {
+      pie: {
+        allowPointSelect: true,
+        cursor: "pointer",
+        dataLabels: {
+          enabled: false,
+        },
+      },
+      series: {
+        enableMouseTracking: false,
+      },
+    },
+    series: [
+      {
+        name: "Brands",
+        colorByPoint: true,
+        data: [
+          {
+            name:
+              "Total Amount Payable <br>₹" +
+              Math.round(renderObj.emiData.totalAmount) +
+              "*",
+            y: renderObj.emiData.totalAmount,
+            sliced: true,
+            selected: true,
+            color: renderObj.color.blue,
+          },
+          {
+            name:
+              "Total Interest Payable <br>₹" +
+              Math.round(renderObj.emiData.totalInterest) +
+              "*",
+            y: renderObj.emiData.totalInterest,
+            color: renderObj.color.yellow,
+          },
+        ],
+      },
+    ],
+    exporting: false,
+  };
+
+  const chartObj = renderChart(chartContainerEl, chartOptions);
 
   function calculateEMI(principalAmount, numberOfYears, interestPercent) {
     let durationInMonths = numberOfYears * 12;
@@ -64,7 +130,27 @@ const renderObj = {};
     };
   }
 
-  renderObj.calcData = calcData;
+  function renderChart(chartEl, chartOptions) {
+    return Highcharts.chart(chartEl, chartOptions);
+  }
+
+  function renderEmiData(emiAmount, totalAmount, interestAmount) {
+    emiAmountEl.innerText =
+      "₹" + Math.round(emiAmount).toLocaleString("en-IN") + "*";
+
+    totalAmountEl.innerText =
+      "₹" + Math.round(totalAmount).toLocaleString("en-IN") + "*";
+
+    totalInterestEl.innerText =
+      "₹" + Math.round(interestAmount).toLocaleString("en-IN") + "*";
+  }
+
+  function checkInputClass(inputEl, className, updatedData) {
+    if (inputEl.classList.contains(className)) {
+      updatedData = inputEl.value;
+      console.log(inputEl, className, updatedData);
+    }
+  }
 
   function updateInputVal(inputEl, rangeSliderEl) {
     // set range slider's default value to input's default value
@@ -73,6 +159,16 @@ const renderObj = {};
     // update input value on slider change
     rangeSliderEl.addEventListener("input", function (event) {
       inputEl.value = event.target.value;
+
+      // checkInputClass(this, "loan-amount-range", renderObj.calcData.amount);
+
+      // checkInputClass(this, "loan-duration-range", renderObj.calcData.duration);
+
+      // checkInputClass(
+      //   this,
+      //   "loan-interest-range",
+      //   renderObj.calcData.interestRate
+      // );
 
       if (this.classList.contains("loan-amount-range")) {
         renderObj.calcData.amount = this.value;
@@ -83,37 +179,80 @@ const renderObj = {};
       }
 
       renderObj.emiData = calculateEMI(
-        parseFloat(calcData.amount),
-        parseFloat(calcData.duration),
-        parseFloat(calcData.interestRate)
+        Number(renderObj.calcData.amount),
+        Number(renderObj.calcData.duration),
+        Number(renderObj.calcData.interestRate)
       );
 
-      emiAmountEl.innerText = "₹" + Math.round(renderObj.emiData.emi) + "*";
-      pieChart.update();
+      renderEmiData(
+        renderObj.emiData.emi,
+        renderObj.emiData.totalAmount,
+        renderObj.emiData.totalInterest
+      );
+
+      chartOptions.series[0].data[0].y = renderObj.emiData.totalAmount;
+      chartOptions.series[0].data[1].y = renderObj.emiData.totalInterest;
+      Highcharts.chart(chartContainerEl, chartOptions);
+
+      console.log(renderObj.calcData);
     });
 
+    // update slider value on input change
     inputEl.addEventListener("change", function (event) {
       if (event.target.value !== "") {
+        // when input value is less than slider min val
         if (
           Math.floor(event.target.value) >
           Math.floor(rangeSliderEl.getAttribute("max"))
         ) {
           event.target.value = rangeSliderEl.getAttribute("max");
-        } else if (
+        }
+
+        // when input value is less than slider min val
+        else if (
           Math.floor(event.target.value) <
           Math.floor(rangeSliderEl.getAttribute("min"))
         ) {
           event.target.value = rangeSliderEl.getAttribute("min");
         }
-      } else {
+      }
+
+      // when input value is empty
+      else {
         event.target.value = rangeSliderEl.getAttribute("min");
         rangeSliderEl.value = event.target.value;
+
+        if (this.classList.contains("loan-amount-input")) {
+          renderObj.calcData.amount = this.value;
+        } else if (this.classList.contains("loan-duration-input")) {
+          renderObj.calcData.duration = this.value;
+        } else {
+          renderObj.calcData.interestRate = this.value;
+        }
       }
 
       rangeSliderEl.value = event.target.value;
-      console.log(amount, rangeSliderEl.value);
+
+      renderObj.emiData = calculateEMI(
+        parseFloat(renderObj.calcData.amount),
+        parseFloat(renderObj.calcData.duration),
+        parseFloat(renderObj.calcData.interestRate)
+      );
+
+      renderEmiData(
+        renderObj.emiData.emi,
+        renderObj.emiData.totalAmount,
+        renderObj.emiData.totalInterest
+      );
+
+      chartOptions.series[0].data[0].y = renderObj.emiData.totalAmount;
+      chartOptions.series[0].data[1].y = renderObj.emiData.totalInterest;
+      Highcharts.chart(chartContainerEl, chartOptions);
+
+      console.log(renderObj.calcData);
     });
 
+    // update slider value on input value change
     inputEl.addEventListener("input", function (event) {
       if (event.target.value !== "") {
         rangeSliderEl.value = event.target.value;
